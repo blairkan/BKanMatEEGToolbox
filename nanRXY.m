952c8155406567ffecd0d460f132ba90da6c702d
Blair Kaneshiro <blairbo@gmail.com>
2020-12-17 14:48:34 -0800

    allow for 4 outputs

--- nanRXY.m
+++ nanRXY.m
@@ -1,4 +1,4 @@
-function [Rxy,Rxx,Ryy] = nanRXY(X,Y)
+function [Rxy,Rxx,Ryy,Ryx] = nanRXY(X,Y)
 
 % This software is licensed under the 3-Clause BSD License (New BSD License), 
 % as follows:
@@ -36,6 +36,7 @@ RXY=nancov([X.' Y.'],'pairwise');
 Rxx=RXY(1:D,1:D);
 Ryy=RXY(D+1:end,D+1:end);
 Rxy=RXY(1:D,D+1:end);
+if nargout==4, Ryx=RXY(D+1:end,1:D); end
 
 % D=size(X,1);
 % RXY=nancov([X.' Y.'],'pairwise');
