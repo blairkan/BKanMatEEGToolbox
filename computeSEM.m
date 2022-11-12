1d179eb81c06f80dbe8b4c3c74f486f6fa8c6fb2
Blair Kaneshiro <blairbo@gmail.com>
2022-11-11 17:33:29 -0900

    first implementation

---
+++ computeSEM.m
@@ -0,0 +1,37 @@
+function sem = computeSEM(x, dim)
+% sem = computeSEM(x)
+% --------------------------
+% Creator: Blair Kaneshiro (Nov 2022)
+%
+% This function takes in a vector or matrix of data, and computes the
+% standard error of the mean.
+%
+% For N observations, SEM = std(x) / sqrt(N)
+%
+% Inputs:
+% - x (required): Input data matrix or vector
+% - dim (optional): For 2d input data, along which dimension to compute
+%   SEM. If the input x is a matrix *and* this input is empty or not
+%   entered, the function will compute SEM along the row dimension (one
+%   value per column of data).
+
+%% Check inputs
+
+assert(nargin >= 1, 'At least one input, x, is required.');
+
+%% Compute SEM
+
+% If input is a vector
+if isvector(x)
+    sem = std(x) / sqrt(length(x));
+
+% If input is a matrix
+else
+    
+    if nargin < 2 || isempty(dim), dim = 1; end
+    
+    s = std(x, [], dim);
+    l = size(x, dim);
+    sem = s / sqrt(l);
+    
+end
