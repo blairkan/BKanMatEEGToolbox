28178834b9be7288bdf331e1824319f6870ca9ab
Blair Kaneshiro <blairbo@gmail.com>
2024-03-19 15:21:31 -0800

    init new function

---
+++ computeMatrixPctOverThresh.m
@@ -0,0 +1,7 @@
+function pctOverThresh = computeMatrixPctOverThresh(xIn, thresh)
+% pctOverThresh = computeMatrixPctOverThresh(xIn, thresh)
+% -------------------------------------------------
+% Blair - March 19, 2024
+%
+% This matrix computes the percent of (magnitude) values in a matrix that
+% are over a specified threshold. 
